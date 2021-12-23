36434eccecefe9c34457476a1d0f26563eb20f31
Senthil Naygam <93111604+senthilnayagam123@users.noreply.github.com>
2021-12-23 11:56:03 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,12 +3,12 @@ public static void main(String[] args){
 int a = 10;
 int b = 20;
 int result = (a+b)*(a-b);
-int count = 1;
+
 for(int i = 1; i<5;i++){
 System.out.println(result);
-//a++;
-//b++;
-//count++;
+ a++;
+ b++;
+
 }
 }
 }
